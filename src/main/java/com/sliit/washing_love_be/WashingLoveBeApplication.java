8285ef24a6cf4df09a3f3f140f06c87d8b4b9e7f
Thilina Navodana <thilinanavodana@gmail.com>
2023-05-21 12:13:28 +0530

    initial commit

---
+++ src/main/java/com/sliit/washing_love_be/WashingLoveBeApplication.java
@@ -0,0 +1,13 @@
+package com.sliit.washing_love_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WashingLoveBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WashingLoveBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sliit/washing_love_be/WashingLoveBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sliit.washing_love_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WashingLoveBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
